25b91a59b3427f7a28464332ad8d3d80b68f2acb
Maryimmary <maryimmary@yandex.ru>
2017-08-24 12:20:03 +0300

    Галерея

--- js/pictures.js
+++ js/pictures.js
@@ -1,16 +1,6 @@
-//Массив номеров 1-25
+'use strict';
 
-var randomPhotoNumbers = function () {
-  var photoNumbers = [];
-  var photoNumbersSize = 25;
-  for (var i = 1; i <= photoNumbersSize; i++) {
-    photoNumbers.push(i);
-  }
-  return photoNumbers[i];
-};
-
-//Cлучайное число лайков
-
+// Cлучайное число лайков
 var randomlikeNumbers = function () {
   var likeNumbers = [];
   var likeNumbersCount = 200;
@@ -20,8 +10,7 @@ var randomlikeNumbers = function () {
   return likeNumbers[Math.floor(Math.random() * likeNumbers.length)];
 };
 
-//Комментарии
-
+// Комментарии
 var randomComment = function () {
   var commentAll = [
     'Всё отлично!',
@@ -34,12 +23,44 @@ var randomComment = function () {
   return commentAll[Math.floor(Math.random() * commentAll.length)];
 };
 
-//Массив из 25 объектов
-var parameterAll = [
-  {
-    url: 'photos/{{randomPhotoNumbers().toString()}}.jpg',
-    likes: randomlikeNumbers(),
-    comments: randomComment().toString()
+// Массив из 25 объектов
+var parameterAllFunction = function () {
+  var parameterAll = [];
+  var parameterAllObjects = 25;
+  for (var i = 1; i <= parameterAllObjects; i++) {
+    param = {
+      url: 'photos/' + i + '.jpg',
+      likes: randomlikeNumbers(),
+      comments: randomComment()
+    };
+    parameterAll.push(param);
   }
-];
-console.log(parameterAll);
+  return parameterAll;
+};
+
+var pictureTemplate = document.querySelector('#picture-template').content;
+var picturesField = document.querySelector('.pictures');
+
+var pictureAdd = function() {
+  var pictureContent = pictureTemplate.cloneNode(true);
+  pictureContent.querySelector('img').getAttribute('src').textContent = parameterAll[param].url;
+  pictureContent.querySelector('.picture-likes').textContent = parameterAll[param].likes;
+  pictureContent.querySelector('.picture-comments').textContent = parameterAll[param].comments;
+  return pictureContent;
+}
+
+var fragment = document.createDocumentFragment();
+for (var i = 1; i <= parameterAll.length; i++) {
+  fragment.appendChild(pictureAdd(parameterAll[i]));
+}
+picturesField.appendChild(fragment);
+
+var uploadOverlay = document.querySelector('.upload-overlay');
+uploadOverlay.classList.remove('hidden');
+
+var galleryOverlay = document.querySelector('.gallery-overlay');
+galleryOverlay.classList.remove('hidden');
+
+galleryOverlay.querySelector('.gallery-overlay-image').getAttribute('src').textContent = parameterAll[1].url;
+galleryOverlay.querySelector('.likes-count').textContent = parameterAll[1].likes;
+galleryOverlay.querySelector('.comments-count').textContent = parameterAll[1].comments;
