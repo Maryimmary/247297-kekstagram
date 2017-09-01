06a545083cf4fc58f214df19992b248b0e5002c9
Maryimmary <maryimmary@yandex.ru>
2017-09-01 20:58:40 +0300

    Ресайз и фильтры

--- js/pictures.js
+++ js/pictures.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var randomLikeNumbers = function (min, max) {
   var randomLike = min - 0.5 + Math.random() * (max - min + 1);
   randomLike = Math.round(randomLike);
@@ -50,9 +53,6 @@ for (var i = 0; i < parameterObjects.length; i++) {
 }
 picturesField.appendChild(fragment);
 
-var uploadOverlay = document.querySelector('.upload-overlay');
-uploadOverlay.classList.add('hidden');
-
 var galleryOverlay = document.querySelector('.gallery-overlay');
 var galleryOverlayClose = document.querySelector('.gallery-overlay-close');
 
@@ -60,12 +60,9 @@ var galleryOverlayShow = function (number) {
   galleryOverlay.querySelector('.gallery-overlay-image').setAttribute('src', parameterObjects[number].url.toString());
   galleryOverlay.querySelector('.likes-count').textContent = parameterObjects[number].likes;
   galleryOverlay.querySelector('.comments-count').textContent = parameterObjects[number].comments;
-  return galleryOverlay;
 };
 
 // Пользовательский интерфейс
-var ESC_KEYCODE = 27;
-var ENTER_KEYCODE = 13;
 var picture = document.querySelectorAll('.picture');
 
 // Функции
@@ -111,3 +108,94 @@ var setListener = function (index) {
 for (var index = 0; index < picture.length; index++) {
   setListener(index);
 }
+
+// ВАЛИДАЦИЯ ФОРМЫ
+
+var uploadFormImage = document.querySelector('.upload-image'); // Блок загрузки
+var uploadFile = document.getElementById('upload-file'); // Input загрузки  файлов
+var uploadOverlay = document.querySelector('.upload-overlay'); // Окно изменений
+var uploadFormCancel = document.getElementById('upload-cancel'); // Кнопка закрытия окна изменений
+var userCommentForm = document.querySelector('.upload-form-description');
+var resizeControlValue = uploadOverlay.querySelector('.upload-resize-controls-value');
+var resizeButtonMin = uploadOverlay.querySelector('.upload-resize-controls-button-dec');
+var resizeButtonMax = uploadOverlay.querySelector('.upload-resize-controls-button-inc');
+
+
+// Функции
+var openUploadOverlay = function () {
+  uploadOverlay.classList.remove('hidden');
+  uploadFormImage.classList.add('hidden');
+  document.addEventListener('keydown', onUploadOverlayEscPress);
+};
+
+var closeUploadOverlay = function () {
+  uploadOverlay.classList.add('hidden');
+  uploadFormImage.classList.remove('hidden');
+  document.removeEventListener('keydown', onUploadOverlayEscPress);
+};
+
+// Обработчики
+var onUploadOverlayEscPress = function (evt) {
+  if (document.activeElement !== userCommentForm && evt.keyCode === ESC_KEYCODE) {
+    closeUploadOverlay();
+  }
+};
+
+// Cобытия
+uploadFile.addEventListener('change', openUploadOverlay);
+uploadFormCancel.addEventListener('click', closeUploadOverlay);
+
+// Функция изменения размера фотографии
+var resizeMin = 25;
+var resizeMax = 100;
+var resizeStep = 25;
+var resizeValue = resizeControlValue.getAttribute('value');
+
+var resizeToMin = function () {
+  resizeValue = parseFloat(resizeValue);
+  var resizeNewValue = resizeValue - resizeStep;
+  return resizeNewValue + '%';
+};
+
+var resizeToMax = function () {
+  resizeValue = parseFloat(resizeValue);
+  var resizeNewValue = resizeValue + resizeStep;
+  return resizeNewValue + '%';
+};
+
+var onButtonMinPress = function () {
+  if (resizeValue > resizeMin) {
+    resizeToMin();
+  }
+};
+
+var onButtonMaxPress = function () {
+  if (resizeValue < resizeMax) {
+    resizeToMax();
+  }
+};
+
+resizeButtonMin.addEventListener('click', onButtonMinPress);
+resizeButtonMax.addEventListener('click', onButtonMaxPress);
+
+// ФИЛЬТРЫ
+
+var effectImagePreview = document.querySelector('.effect-image-preview');
+var effectControlsBlock = document.querySelector('.upload-effect-controls'); // Блок всех фильтров
+var uploadEffectButton = effectControlsBlock.getElementsByTagName('input');
+
+var getValueInput = function (event) {
+  var target = event.target;
+  if (target === uploadEffectButton) {
+    var valueInput = uploadEffectButton.getAttribute('value');
+  }
+  return valueInput;
+};
+
+for (i = 0; i < uploadEffectButton.length; i++) {
+  uploadEffectButton[i].addEventListener('click', function () {
+    effectImagePreview.classList.add('effect-' + getValueInput());
+  });
+}
+
+
